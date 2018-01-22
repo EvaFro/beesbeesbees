a699be51657c29ffede4b8e2f9fb95ebb61b491e
EvaFro <frolli.erin@gmail.com>
2018-01-22 12:06:09 -0800

    Completed requierments for both forager bee & Honeymaker Bee.

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,16 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = fucntion() {
+  this.honeyPot++;
+}
+
+HoneyMakerBee.prototype.giveHoney = fucntion() {
+  this.honeyPot--;
+}
